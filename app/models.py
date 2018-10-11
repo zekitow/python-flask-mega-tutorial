1d70b7ec431f6ead996db962c3dfef1fec303230
José Ribeiro <zeca@digup.io>
2018-10-11 20:27:57 -0300

    Added login manager and hash generator.

--- app/models.py
+++ app/models.py
@@ -1,15 +1,32 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from app import db
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id            = db.Column(db.Integer, primary_key=True)
     username      = db.Column(db.String(64), index=True, unique=True)
     email         = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts         = db.relationship('Post', backref='author', lazy='dynamic')
 
+    # 
+    # pretty print on shell
+    # 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    # 
+    # encrypts password hash
+    # 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    # 
+    # check if raw string matches with encrypted hash
+    # 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id        = db.Column(db.Integer, primary_key=True)
@@ -17,5 +34,8 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id   = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    # 
+    # pretty print on shell
+    # 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
