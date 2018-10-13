7dfd86df0e867efe23f7d99d6dc5b46fa5b811c3
José Ribeiro <zeca@digup.io>
2018-10-13 11:44:42 -0300

    Required login to see index.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
 from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = { 'username': 'zek' }
     posts = [
@@ -33,7 +35,12 @@ def login():
             return redirect(url_for('index'))
 
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+        return redirect(next_page)
 
     return render_template('login.html', title='Sign In', form=form)
 
