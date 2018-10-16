d2c90846878d0dea118a36b08e1ac83f6c0dab81
José Ribeiro <zeca@digup.io>
2018-10-16 18:30:31 -0300

    Updating last_seen on page load.

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -70,3 +78,4 @@ def user(username):
     user  = User.query.filter_by(username=username).first_or_404()
     posts = []
     return render_template('user.html', user=user, posts=posts)
+
