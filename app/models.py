f37f20f95cda7db4a2fb85e7d9f1d19558076557
José Ribeiro <zeca@digup.io>
2018-10-16 18:15:19 -0300

    Created simple user profile.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
@@ -27,6 +28,14 @@ class User(UserMixin, db.Model):
     # 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    #
+    # generates avatar using
+    # gravatar to users
+    #
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'http://gravatar.com/avatar/{}?d=identicon&size={}'.format(digest, size)
 
 @login.user_loader
 def load_user(id):
--- app/routes.py
+++ app/routes.py
@@ -63,3 +63,10 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user  = User.query.filter_by(username=username).first_or_404()
+    posts = []
+    return render_template('user.html', user=user, posts=posts)
