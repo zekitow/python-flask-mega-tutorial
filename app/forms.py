6bdaba5dc1a616beaeae19a2be2c331af22fc453
José Ribeiro <zeca@digup.io>
2018-10-23 18:16:45 -0300

    Added i18n support.

--- app/forms.py
+++ app/forms.py
@@ -1,44 +1,45 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, Required, Email, EqualTo, Length
+from flask_babel import _, lazy_gettext as _l
 from app.models import User
 
 class LoginForm(FlaskForm):
-    username    = StringField('Username', validators=[Required()])
-    password    = PasswordField('Password', validators=[Required()])
-    remember_me = BooleanField('Remember me')
-    submit      = SubmitField('Sign In')
+    username    = StringField(_l('Username'), validators=[Required()])
+    password    = PasswordField(_l('Password'), validators=[Required()])
+    remember_me = BooleanField(_l('Remember me'))
+    submit      = SubmitField(_l('Sign In'))
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[Required()])
-    email    = StringField('E-mail', validators=[Required(), Email()])
-    password = PasswordField('Password', validators=[Required()])
-    password2 = PasswordField('Repeat Password', validators=[Required(), EqualTo('password')])
-    submit   = SubmitField('Register')
+    username = StringField(_l('Username'), validators=[Required()])
+    email    = StringField(_l('E-mail'), validators=[Required(), Email()])
+    password = PasswordField(_l('Password'), validators=[Required()])
+    password2 = PasswordField(_l('Repeat Password'), validators=[Required(), EqualTo('password')])
+    submit   = SubmitField(_l('Register'))
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('Username already taken.')
+            raise ValidationError(_l('Username already taken.'))
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('E-mail already taken.')
+            raise ValidationError(_l('E-mail already taken.'))
 
 class ResetPasswordRequestForm(FlaskForm):
-    email  = StringField('Email', validators=[Required(), Email()])
-    submit = SubmitField('Request Password Reset')
+    email  = StringField(_l('Email'), validators=[Required(), Email()])
+    submit = SubmitField(_l('Request Password Reset'))
 
 class ResetPasswordForm(FlaskForm):
-    password  = PasswordField('Password', validators=[Required()])
-    password2 = PasswordField('Repeat Password', validators=[Required(), EqualTo('password')])
-    submit    = SubmitField('Request Password Reset')
+    password  = PasswordField(_l('Password'), validators=[Required()])
+    password2 = PasswordField(_l('Repeat Password'), validators=[Required(), EqualTo('password')])
+    submit    = SubmitField(_l('Request Password Reset'))
 
 class EditProfileForm(FlaskForm):
-    username = StringField('Username', validators=[Required()])
-    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
-    submit   = SubmitField('Submit')
+    username = StringField(_l('Username'), validators=[Required()])
+    about_me = TextAreaField(_l('About me'), validators=[Length(min=0, max=140)])
+    submit   = SubmitField(_l('Submit'))
 
     def __init__(self, original_username, *args, **kwargs):
         super(EditProfileForm, self).__init__(*args, **kwargs)
@@ -48,8 +49,8 @@ class EditProfileForm(FlaskForm):
         if username.data != self.original_username:
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
-                raise ValidationError('Please use a different username.')
+                raise ValidationError(_l('Please use a different username.'))
 
 class PostForm(FlaskForm):
-    post   = TextAreaField('Say something', validators=[Required(), Length(min=1, max=140)])
-    submit = SubmitField('Submit')
+    post   = TextAreaField(_l('Say something'), validators=[Required(), Length(min=1, max=140)])
+    submit = SubmitField(_l('Submit'))
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from werkzeug.urls import url_parse
+from flask_babel import _, get_locale
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
@@ -12,6 +13,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -22,7 +24,7 @@ def index():
         post = Post(body=form.post.data, author=current_user)
         db.session.add(post)
         db.session.commit()
-        flash('Your post is now live!')
+        flash(_('Your post is now live!'))
         return redirect(url_for('index'))
 
     page  = request.args.get('page', 1, type=int)
@@ -31,7 +33,7 @@ def index():
     next_url = url_for('index', page=posts.next_num) if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
 
-    return render_template('index.html', title='Home', posts=posts.items, \
+    return render_template('index.html', title=_('Home'), posts=posts.items, \
         form=form, next_url=next_url, prev_url=prev_url)
 
 @app.route('/explore')
@@ -43,7 +45,7 @@ def explore():
     next_url = url_for('index', page=posts.next_num) if posts.has_next else None
     prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
 
-    return render_template('index.html', title='Explore', posts=posts.items, \
+    return render_template('index.html', title=_('Explore'), posts=posts.items, \
         next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET','POST'])
@@ -55,7 +57,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+            flash(_('Invalid username or password'))
             return redirect(url_for('index'))
 
         login_user(user, remember=form.remember_me.data)
@@ -66,7 +68,7 @@ def login():
 
         return redirect(next_page)
 
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title=_('Sign In'), form=form)
 
 @app.route('/logout')
 def logout():
@@ -84,10 +86,10 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('User successfully created!')
+        flash(_('User successfully created!'))
         return redirect(url_for('login'))
 
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title=_('Register'), form=form)
 
 @app.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
@@ -102,10 +104,10 @@ def reset_password_request():
         if user:
             send_password_reset_email(user)
 
-        flash('Check your email for the instructions to reset your password')
+        flash(_('Check your email for the instructions to reset your password'))
         return redirect(url_for('login'))
 
-    return render_template('reset_password_request.html', title='Reset Password', form=form)
+    return render_template('reset_password_request.html', title=_('Reset Password'), form=form)
 
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
@@ -120,7 +122,7 @@ def reset_password(token):
     if form.validate_on_submit():
         user.set_password(form.password.data)
         db.session.commit()
-        flash('Your password has been reset.')
+        flash(_('Your password has been reset.'))
         return redirect(url_for('login'))
 
     return render_template('reset_password.html', form=form)
@@ -147,12 +149,12 @@ def edit_profile():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
-        flash('Your changes have been saved.')
+        flash(_('Your changes have been saved.'))
         return redirect(url_for('edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title='Edit profile', form=form)
+    return render_template('edit_profile.html', title=_('Edit profile'), form=form)
 
 @app.route('/follow/<username>')
 @login_required
@@ -160,16 +162,16 @@ def follow(username):
     user = User.query.filter_by(username=username).first()
 
     if user is None:
-        flash('User {} not found'.format(username))
+        flash(_('User {} not found'.format(username)))
         return redirect(url_for('index'))
 
     if user == current_user:
-        flash('You cannot follow yourself.')
+        flash(_('You cannot follow yourself.'))
         return redirect(url_for('user', username=username))
 
     current_user.follow(user)
     db.session.commit()
-    flash('You are now following {}'.format(username))
+    flash(_('You are now following {}'.format(username)))
     return redirect(url_for('user', username=username))
 
 @app.route('/unfollow/<username>')
@@ -178,14 +180,14 @@ def unfollow(username):
     user = User.query.filter_by(username=username).first()
 
     if user is None:
-        flash('User {} not found'.format(username))
+        flash(_('User {} not found'.format(username)))
         return redirect(url_for('index'))
 
     if user == current_user:
-        flash('You cannot unfollow yourself.')
+        flash(_('You cannot unfollow yourself.'))
         return redirect(url_for('user', username=username))
 
     current_user.unfollow(user)
     db.session.commit()
-    flash('You are not following {} anymore'.format(username))
+    flash(_('You are not following {} anymore'.format(username)))
     return redirect(url_for('user', username=username))
