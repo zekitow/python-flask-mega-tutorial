e809bedb5b15615d77c942eb0801394d6b228f4b
José Ribeiro <zeca@digup.io>
2018-11-08 07:15:21 -0200

    Added translations to posts.

--- app/models.py
+++ app/models.py
@@ -112,6 +112,7 @@ class Post(db.Model):
     body      = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id   = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language  = db.Column(db.String(5))
 
     # 
     # pretty print on shell
